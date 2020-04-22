40428109fa20ef83b4593f6d899247c5ea87d828
CFCode-git <435184766@qq.com>
2020-04-22 08:54:18 +0800

    deploy

---
+++ precache-manifest.e67c5962566c404fa23ea72949083c21.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b3d063c7b270166b359",
+    "url": "/moneygrubber2.0-website/css/app.b2299408.css"
+  },
+  {
+    "revision": "adbd2589b46d78ac5d78794bf32e3a9d",
+    "url": "/moneygrubber2.0-website/gitee_qrcode2.png"
+  },
+  {
+    "revision": "064491a9a1c7a1cece6abb4309f0fffb",
+    "url": "/moneygrubber2.0-website/index.html"
+  },
+  {
+    "revision": "3b3d063c7b270166b359",
+    "url": "/moneygrubber2.0-website/js/app.7f6f40c1.js"
+  },
+  {
+    "revision": "147316236e2ee02c4d5c",
+    "url": "/moneygrubber2.0-website/js/chunk-vendors.27b71b12.js"
+  },
+  {
+    "revision": "903788e72e1163216aa9d7bd6df928f6",
+    "url": "/moneygrubber2.0-website/manifest.json"
+  },
+  {
+    "revision": "feb6ca58e70cc13ecaf40294b3c9b63f",
+    "url": "/moneygrubber2.0-website/qrcode2.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/moneygrubber2.0-website/robots.txt"
+  }
+]);
